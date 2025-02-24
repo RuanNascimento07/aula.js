ee8cf9bcfe16c8118fb3d09c8527189ea0dd8a33
RuanNascimento07 <162449821+RuanNascimento07@users.noreply.github.com>
2025-02-24 08:50:36 -0300

    Add files via upload

---
+++ boolean.js
@@ -0,0 +1,18 @@
+//OPERADORES DE COMPARAÇÃO
+// Operadoes de igualdade (compara apenas o valor)
+console.log("1" == 1)
+console.log(true == 1)
+console.log(false == 0)
+console.log(false == "")
+
+// Oprador de igualade estrita
+// (compra valor e tipo)
+console.log("1" === 1)
+console.log(true === 1)
+console.log(false === 0)
+
+//Verificar tipo da variavel
+console.log(typeof ("1")) // -> texto (string)
+console.log(typeof (1)) // -> Number
+console.log(typeof (true)) // -> boolean
+
