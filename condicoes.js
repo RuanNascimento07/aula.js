76c5e2b086d91b28dc2a3c8677badf70a8f14f30
RuanNascimento07 <162449821+RuanNascimento07@users.noreply.github.com>
2025-02-25 12:17:15 -0300

    Add files via upload

---
+++ condicoes.js
@@ -0,0 +1,11 @@
+let idade =  18
+let possuiCNH = true
+if (idade >= 18){
+    if (possuiCNH == true){
+        console.log("Você pode dirigir!")
+    } else {
+    console.log ("Vocẽ pode tirar CNH!")
+    }
+} else {
+    console.log("Não pode")
+}
---
+++ exercicio.js
@@ -0,0 +1,13 @@
+let peso = 69
+let qtd_agua = peso * 35/1000
+console.log("Quantidade de Água = " + qtd_agua)
+
+
+
+let valor1 = 10.0
+let valor2 =  6.0
+let valor3 = 4.0
+let soma = valor1 +  valor2 + valor3
+let media = soma /3
+console.log("media = " + media)
+
