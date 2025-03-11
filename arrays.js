d6f39d7ee40f2b265f4eea35759bed83d7e3399e
RuanNascimento07 <162449821+RuanNascimento07@users.noreply.github.com>
2025-03-11 11:59:13 -0300

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,16 @@
+const lanches = ["x-egg","x-ovo", "x-bacon", "x-tudo", "x-frango"];
+const precos = [12,15,15,20,12];
+console.log(lanches);
+console.log(precos);
+
+console.log(lanches);
+console.log(precos);
+console.log(lanches[0]+ " - R$ " + precos [0])
+console.log(lanches[1]+ " - R$ " + precos [1])
+console.log(lanches[2]+ " - R$ " + precos [2])
+console.log(lanches[3]+ " - R$ " + precos [3])
+console.log(lanches[4]+ " - R$ " + precos [4])
+
+// Encontrar Tamanho da Lista:
+const tamanho = lanches.length;
+console.log(tamanho)
---
+++ lacoRepeticao.js
@@ -0,0 +1,13 @@
+// LACO DE REPETIÇÃO
+// Laço WHILE - ENQUANTO
+
+let numero = 1;
+while(numero < 10){
+console.log(numero);
+numero = numero + 1;}
+
+//imprimir o  cardapio usando laco while
+while (contador <tam -1){
+    //x-egg - R$12
+    contador = contador + 1
+}
